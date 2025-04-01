968513873e220724e968ac4f82c5ee5af88a53ed
bhun0002 <bhungaliyadeval@gmail.com>
2025-03-31 21:35:52 -0400

    Commit Main changes

--- infra/tf-backend/main.tf
+++ infra/tf-backend/main.tf
@@ -6,43 +6,43 @@ terraform {
     }
   }
 }
- 
+
 provider "azurerm" {
   features {}
 }
- 
+
 resource "azurerm_resource_group" "backend_rg" {
   name     = "bhun0002bhup0006-githubactions-rg"
   location = "East US"
 }
- 
+
 resource "azurerm_storage_account" "backend_sa" {
-  name                     = "bhun0002bhup0006githubactions"
+  name                     = "bhun0002bhup0006ghact"
   resource_group_name      = azurerm_resource_group.backend_rg.name
   location                 = azurerm_resource_group.backend_rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   min_tls_version          = "TLS1_2"
 }
- 
+
 resource "azurerm_storage_container" "tfstate" {
   name                  = "tfstate"
   storage_account_name  = azurerm_storage_account.backend_sa.name
   container_access_type = "private"
 }
- 
+
 output "resource_group_name" {
   value = azurerm_resource_group.backend_rg.name
 }
- 
+
 output "storage_account_name" {
   value = azurerm_storage_account.backend_sa.name
 }
- 
+
 output "container_name" {
   value = azurerm_storage_container.tfstate.name
 }
- 
+
 output "arm_access_key" {
   value     = azurerm_storage_account.backend_sa.primary_access_key
   sensitive = true
