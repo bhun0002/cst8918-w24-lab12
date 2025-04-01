6b0a09b8eb0b70d463e3957401bae46f9632be03
bhun0002 <bhungaliyadeval@gmail.com>
2025-03-31 23:30:36 -0400

    Created VNet

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -7,3 +7,22 @@ resource "azurerm_resource_group" "app_rg" {
     environment = "prod"
   }
 }
+
+
+
+resource "azurerm_virtual_network" "app_vnet" {
+  name                = "bhun0002bhup0006-a12-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.app_rg.location
+  resource_group_name = azurerm_resource_group.app_rg.name
+  tags = {
+    environment = "prod"
+  }
+}
+
+resource "azurerm_subnet" "app_subnet" {
+  name                 = "bhun0002bhup0006-a12-subnet"
+  resource_group_name  = azurerm_resource_group.app_rg.name
+  virtual_network_name = azurerm_virtual_network.app_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
